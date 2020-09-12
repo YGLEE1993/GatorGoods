eaea08aa930d4a249504a2a892d04db28d1f17ef
Eastae <keastman@mail.sfsu.edu>
2020-09-11 18:39:27 -0700

    added static routing

--- application/server/server.js
+++ application/server/server.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
 const cors = require('cors');
 const mysql = require("mysql");
 const app = express();
-const port = process.env.PORT || 3000;
- 
+const port = 8080;
+
+//Serve static files from React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+
+// Handles any requests
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
