1a9b234d2949cf65a17fe75ae928643c947e1b39
YGLEE1993 <yugyeonglee93@gmail.com>
2020-10-22 23:03:00 -0700

    test

--- application/server/server.js
+++ application/server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+const path = require("path");
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -13,5 +14,6 @@ app.use(express.static(path.join(__dirname, "../client/build")));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
 });
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up on port ${port}...`));
