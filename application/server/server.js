aeb7d73a3ab792858279c5b8077775cf5b542795
YGLEE1993 <yugyeonglee93@gmail.com>
2020-10-22 21:15:03 -0700

    testing if we need to connect / to server

--- application/server/server.js
+++ application/server/server.js
@@ -10,6 +10,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api/product", require("./routers/productRouter"));
 app.use("/api/search", require("./routers/searchRouter"));
 app.use(express.static(path.join(__dirname, "../client/build")));
-
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up on port ${port}...`));
