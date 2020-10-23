76277c240677e0c9b13c3e112345455bf7182483
YGLEE1993 <yugyeonglee93@gmail.com>
2020-10-22 22:13:08 -0700

    server.js path resolve

--- application/server/server.js
+++ application/server/server.js
@@ -10,8 +10,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api/product", require("./routers/productRouter"));
 app.use("/api/search", require("./routers/searchRouter"));
 app.use(express.static(path.join(__dirname, "../client/build")));
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
 });
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up on port ${port}...`));
