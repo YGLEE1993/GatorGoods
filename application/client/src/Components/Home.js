2de20cd5e4952fd463547d183c888ee3bb3db8e8
Joy <zhuozhuo530@gmail.com>
2020-10-30 23:14:16 -0700

    make the card clickable and redirect to product listing

--- application/client/src/Components/Home.js
+++ application/client/src/Components/Home.js
@@ -6,42 +6,43 @@ import ListingCard from "./ListingCard"
 
 
 export default function Home() {
-  return (
-    <div>
-        <Jumbotron>
-            <div className="Jumbotron">
-                <h1>Hello, Gators! </h1>
-                <p>
-                    Welcome to the online marketplace specifically designed for students, faculty and staff at SFSU.
-                </p>
-            </div>
-        </Jumbotron>
-        
-        <Featured />
+    
+    return (
+        <div>
+            <Jumbotron>
+                <div className="Jumbotron">
+                    <h1>Hello, Gators! </h1>
+                    <p>
+                        Welcome to the online marketplace specifically designed for students, faculty and staff at SFSU.
+                    </p>
+                </div>
+            </Jumbotron>
+            
+            <Featured />
 
-        <Container style={{marginTop: "2rem", paddingLeft: 0, paddingRight: 0}}>
-            <Col>
-                <Row >
-                    <Col><h3>Books</h3></Col>
-                    <Col lg={9}><p>45 listings for this category</p></Col>
-                    <Col className="text-right">
-                        <Button variant="secondary" href="/category">See more</Button>{' '}
-                    </Col>
-                </Row>
-                <Row className="card-container">
-                    <CardDeck className="justify-content-lg-center">
-                        <ListingCard />
-                        <ListingCard />
-                        <ListingCard />
-                        <ListingCard />
-                        <ListingCard />
-                        <ListingCard />
-                        <ListingCard />
-                        <ListingCard />
-                    </CardDeck>
-                </Row>
-            </Col>
-        </Container>
-    </div>
+            <Container style={{marginTop: "2rem", paddingLeft: 0, paddingRight: 0}}>
+                <Col>
+                    <Row >
+                        <Col><h3>Books</h3></Col>
+                        <Col lg={9}><p>45 listings for this category</p></Col>
+                        <Col className="text-right">
+                            <Button variant="secondary" href="/category">See more</Button>{' '}
+                        </Col>
+                    </Row>
+                    <Row className="card-container">
+                        <CardDeck className="justify-content-lg-center">
+                            <ListingCard />
+                            <ListingCard />
+                            <ListingCard />
+                            <ListingCard />
+                            <ListingCard />
+                            <ListingCard />
+                            <ListingCard />
+                            <ListingCard />
+                        </CardDeck>
+                    </Row>
+                </Col>
+            </Container>
+        </div>
   );
 }
--- application/client/src/Components/ListingCard.js
+++ application/client/src/Components/ListingCard.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Card, Col, Row } from "react-bootstrap";
 import "./ListingCard.css"
+import { useHistory } from "react-router-dom"
 
 
 export default function ListingCard () {
+    const history = useHistory();
+    function handleClick() {
+        history.push("/productlisting");
+    }
+    
     return (
         <div>
-            <Card className="listingcard">
+            <Card onClick={handleClick}  className="listingcard">
                 <Card.Img variant="top" src="holder.js/100px160" />
                 <Card.Body>
                     <Card.Title>Listing title</Card.Title>
