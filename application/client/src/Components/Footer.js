f76cb7d1bc05bb71ec6218d53e6f608b43016d32
Joy <zhuozhuo530@gmail.com>
2020-11-05 16:35:18 -0800

    added a footer to all pages

---
+++ application/client/src/Components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Container, Row } from "react-bootstrap"
+
+export default function Footer() {
+    return (
+        <Container style={{marginTop: "5rem", marginBottom:"1rem"}}>
+            <Row className="justify-content-lg-center" >
+            <div>
+                <footer class="footer">
+                    <div>
+                    <a href="./about">CSC 648-848 Global Team</a>
+                    <span> &copy; 2020</span>
+                    </div>
+                </footer>
+            </div>
+            </Row>
+        </Container>
+    )
+}
