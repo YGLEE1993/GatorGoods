1cba3b659d8843086193bbef6bd0d20b9a8bdfe6
Joy <zhuozhuo530@gmail.com>
2020-11-05 17:28:54 -0800

    added images of featured categories

--- application/client/src/Components/Featured.js
+++ application/client/src/Components/Featured.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { Container, CardDeck, Card } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
-import "./Feature.css"
+import "./Feature.css";
+import furniture from "./assets/furniture.jpeg";
+import book from "./assets/book.jpeg";
+import electronics from "./assets/electronics.jpeg";
+import others from "./assets/others.jpeg";
 
 export default function Featured(props) {
   const history = useHistory();
@@ -21,25 +25,25 @@ export default function Featured(props) {
         <h2>Featured Categories</h2>
         <CardDeck style={{ padding: "2rem" }}>
           <Card onClick={handleBook} style={{ border: 0 }} className="category-card">
-            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Img variant="top" src={book} />
             <Card.Body>
               <Card.Title>Books</Card.Title>
             </Card.Body>
           </Card>
           <Card onClick={handleClick} style={{ border: 0 }} className="category-card">
-            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Img variant="top" src={furniture} />
             <Card.Body>
               <Card.Title>Furniture</Card.Title>
             </Card.Body>
           </Card>
           <Card onClick={handleClick} style={{ border: 0 }} className="category-card">
-            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Img variant="top" src={electronics} />
             <Card.Body>
               <Card.Title>Electronics</Card.Title>
             </Card.Body>
           </Card>
           <Card onClick={handleClick} style={{ border: 0 }} className="category-card">
-            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Img variant="top" src={others} />
             <Card.Body>
               <Card.Title>Others</Card.Title>
             </Card.Body>
