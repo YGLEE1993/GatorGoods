d577f63bbb90f3ad1f6099c24348698e215a706f
Keith Eastman <eastman.ae@gmail.com>
2020-12-13 17:51:30 -0800

    added google analytics via html script and refactored analytics code in app.js

--- application/client/src/App.js
+++ application/client/src/App.js
@@ -1,35 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navigation from "./Components/views/Navigation/Navigation";
 import Routes from "./Routes";
 import WarningBanner from "./Components/WarningBanner";
 import "bootswatch/dist/pulse/bootstrap.min.css";
 import Footer from "./Components/Footer";
-
-
-/*
-Potential implementation #1 for Google Analytics - not verified yet
- */
-// import ReactDOM from 'react-dom';
-// import ReactGA from 'react-ga';
-// import { createBrowserHistory } from 'history';
-//
-// const history = createBrowserHistory();
-//
-// // Initialize google analytics page view tracking
-// history.listen(location => {
-//     ReactGA.set({ page: location.pathname }); // Update the user's current page
-//     ReactGA.pageview(window.location.pathname); // Record a pageview for the given page
-// });
-
-/*
-Potential implementation #2 for Google Analytics - not verified yet
- */
-import ReactGA from 'react-ga';
-ReactGA.initialize('G-0ECDWLTTK8');
-ReactGA.pageview(window.location.pathname + window.location.search);
-
-
 /**
  * File name: App.js
  * Purpose: App.js is the base structure for the application. From here, we load app-wide components like the our
@@ -41,6 +16,9 @@ ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
   return (
+    
+
+    
     <div className="App">
       {/* <Router history={history}> // part of method #1 for Google Analytics attempt */}
       <Router>
